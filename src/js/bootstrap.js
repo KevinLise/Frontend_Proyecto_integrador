bdd704f7f21f8cbccb5dc8bd892b6c348fe7ec59
Sergio Tobias <tobiasviloriasergiodavid@gmail.com>
2026-07-16 21:19:17 -0500

    build: inicializar npm, instalar Bootstrap 5.3.3 y Vite

---
+++ src/js/bootstrap.js
@@ -0,0 +1,4 @@
+// Entry point de Bootstrap para Vite.
+// Importa Bootstrap JS (incluye Popper automáticamente) y el CSS global.
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
